dc38fd3af34dad508f22d5ad4c86a38f9142141b
Richard <analyticgranger@tuta.com>
2025-09-08 18:14:09 -0700

    Richard - SemicircleDistribution.lean - warmup commit

--- SemicircleLaw/SemicircleDistribution/SemicircleDistribution.lean
+++ SemicircleLaw/SemicircleDistribution/SemicircleDistribution.lean
@@ -604,23 +604,6 @@ lemma h_cont : Continuous h := by
 end Scribbles
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 section Demo
 
 lemma support_semicirclePDF_inc (μ : ℝ) (v : ℝ≥0) :
@@ -690,28 +673,28 @@ lemma integrable_semicirclePDFReal' (μ : ℝ) (v : ℝ≥0) :
   set f := fun x ↦ 1 / (2 * π * v) * √(4 * v - (x - μ) ^ 2)
 
   /- Claim 1: The semicircle pdf f is continuous. -/
-  have h1 : Continuous f := by sorry
+  have h1 : Continuous f := by apply Cont_semicirclePDFReal
 
   /- Define the closed interval I := [μ - 2√{v},μ + 2√v]. -/
   set I := Icc (μ - 2 * √v) (μ + 2 * √v) with hI
 
   /- Claim 2: The closed interval I is compact. -/
-  have h2 : IsCompact I := by sorry
+  have h2 : IsCompact I := by simpa using isCompact_Icc
 
   /- The semicircle pdf f is integrable on the interval I since:
      (a) f is continuous on I <-- h1
      (b) I is a compact subset of ℝ <-- h2
      (c) ℝ is a Hausdorff topological space <-- Lean handles this.
-     (d) the Lebesgue measure is locally finite on I <-- Lean handles this. -/
-  have h3 : IntegrableOn f I := by sorry
+     (d) the Lebesgue measure is locally finite <-- Lean handles this. -/
+  have h3 : IntegrableOn f I := by simpa using (h1.continuousOn).integrableOn_compact h2
 
   /- The support of the semicircle pdf f is contained in the closed interval I. -/
-  have h4 : Function.support f ⊆ I := by sorry
+  have h4 : Function.support f ⊆ I := by apply support_semicirclePDF_inc
 
   /- We are using (the modus ponens/right implication of) the lemma:
-     For a function g whose support is contained in a set s,
-     the function g is integrable on s iff f is integrable on the whole space. -/
-  sorry
+     For a function g whose support is contained in a set s, <-- h4
+     the function g is integrable on s iff f is integrable on the whole space. <-- h3 -/
+  exact (integrableOn_iff_integrable_of_support_subset h4).mp h3
 
 end Demo
 
